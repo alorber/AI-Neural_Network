9319c69b384eebc9a9270b728bbc72d3079a3e13
Andrew Lorber <andrewlorber@aol.com>
2019-11-24 02:23:45 -0500

    Adds fnc to train neural net

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
@@ -42,7 +43,7 @@ public:
         
         // Initializes layers
         Node emptyNode;
-        float weight;
+        double weight;
         
             // Input Layer
         for(int i = 0; i < numInputNodes; i++){
@@ -71,13 +72,145 @@ public:
                 outputLayer.at(i).inputWeights.push_back(weight);
             }
         }
+        fin.close();
     }
     void learn();
     void test();
-    void printWeights();
+    void printWeights(){
+        
+    };
+    double sigmoid(double value){
+        return (1 / (1 + exp(value * (-1))));
+    }
+    double sigmoidPrime(double value){
+        return (sigmoid(value) * (1 - sigmoid(value)));
+    }
 };
+
+void NeuralNetwork::learn(){
+    string trainingFile;
+    string outFile;
+    int numEpochs;
+    double learningRate;
+    
+    cout << "Please enter the name of the file containing the training set.\n";
+    cin >> trainingFile;
+    ifstream fin;
+    fin.open(trainingFile);
+    // Checks for valid file name
+    while(!fin.is_open()){
+        cout << "The file you have entered cannot be found. Please enter a new file name.\n";
+        cin >> trainingFile;
+        fin.open(trainingFile);
+    }
+    
+    cout << "Please enter the name of the output file.\n";
+    cin >> outFile;
+    
+    cout << "Please enter the number of number of epochs.\n";
+    cin >> numEpochs;
+    
+    cout << "Please enter the learning rate.\n";
+    cin >> learningRate;
+    
+    // Loads training examples
+    int numExamples;
+    fin >> numExamples;
+        // Skips to next line
+    fin.ignore(256, '\n');
+    
+        // Each example contains a vector of inputs and a vector of outputs
+    vector<vector<vector<double>>> examples;
+    vector<vector<double>> example;
+    vector<double> eInputs;
+    vector<double> eOutputs;
+    double value;
+    
+    for(int i = 0; i < numExamples; i++){
+        example.clear();
+        eInputs.clear();
+        eOutputs.clear();
+        
+        for(int j = 0; j < numInputNodes; j++){
+            fin >> value;
+            eInputs.push_back(value);
+        }
+        for(int j = 0; j < numOutputNodes; j++){
+            fin >> value;
+            eOutputs.push_back(value);
+        }
+        example.push_back(eInputs);
+        example.push_back(eOutputs);
+        examples.push_back(example);
+    }
+    fin.close();
+    
+    for(int i = 0; i < numEpochs; i++){
+        for(int j = 0; i < numExamples; j++){
+            // Initializes inputs
+            for(int k = 0; k < numInputNodes; k++){
+                inputLayer.at(k).input = inputLayer.at(k).activation = examples.at(j).at(0).at(k);
+            }
+            
+            // Propagate forward to hidden layer
+            double sum;
+            for(int k = 0; k < numHiddenNodes; k++){
+                sum = hiddenLayer.at(k).biasWeight * (-1);
+                for(int l = 0; l < numInputNodes; l++){
+                    sum += hiddenLayer.at(k).inputWeights.at(l) * inputLayer.at(l).activation;
+                }
+                hiddenLayer.at(k).input = sum;
+                hiddenLayer.at(k).activation = sigmoid(sum);
+            }
+            // Propagate forward to output layer
+            for(int k = 0; k < numOutputNodes; k++){
+                sum = outputLayer.at(k).biasWeight * (-1);
+                for(int l = 0; l < numHiddenNodes; l++){
+                    sum += outputLayer.at(k).inputWeights.at(l) * hiddenLayer.at(l).activation;
+                }
+                outputLayer.at(k).input = sum;
+                outputLayer.at(k).activation = sigmoid(sum);
+            }
+            
+            vector<double> outputErrors;
+            vector<double> hiddenErrors;
+            // Calculate error at output layer
+            for(int k = 0; k < numOutputNodes; k++){
+                value = sigmoidPrime(outputLayer.at(k).input)
+                    * (examples.at(j).at(1).at(k) - outputLayer.at(k).activation);
+                outputErrors.push_back(value);
+            }
+            // Propagate backward to hidden layer
+            for(int k = 0; k < numHiddenNodes; k++){
+                sum = 0;
+                for(int l = 0; l < numOutputNodes; l++){
+                    sum += outputLayer.at(l).inputWeights.at(k) * outputErrors.at(l);
+                }
+                value = sigmoidPrime(hiddenLayer.at(k).input) * sum;
+                hiddenErrors.push_back(value);
+            }
+            
+            // Update weights according to deltas
+                // Input layer -> hidden layer
+            for(int k = 0; k < numHiddenNodes; k++){
+                for(int l = 0; l < numInputNodes; l++){
+                    hiddenLayer.at(k).inputWeights.at(l) += (learningRate * inputLayer.at(l).activation * hiddenErrors.at(k));
+                }
+                hiddenLayer.at(k).biasWeight += (learningRate * (-1) * hiddenErrors.at(k));
+            }
+                // Hidden layer -> output layer
+            for(int k = 0; k < numOutputNodes; k++){
+                for(int l = 0; l < numHiddenNodes; l++){
+                    outputLayer.at(k).inputWeights.at(l) += (learningRate * hiddenLayer.at(l).activation * outputErrors.at(k));
+                }
+                outputLayer.at(k).biasWeight += (learningRate * (-1) * outputErrors.at(k));
+            }
+        }
+    }
+}
 
 int main(int argc, const char * argv[]) {
     NeuralNetwork net = NeuralNetwork("init.txt");
+    net.learn();
     return 0;
 }
