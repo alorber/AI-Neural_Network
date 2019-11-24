15a4d0944177159a28ff382141a782ef923dfb89
Andrew Lorber <andrewlorber@aol.com>
2019-11-23 19:20:51 -0500

    Init commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AI Neural Network Project
+//
+//  Created by Andrew Lorber on 11/23/19.
+//  Copyright © 2019 Andrew Lorber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
